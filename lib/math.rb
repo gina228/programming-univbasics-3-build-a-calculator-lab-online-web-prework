e671ded35d7fef987dda29524fd42bc9a07ab510
Gina Graci <ginagraci92@gmail.com>
2019-09-16 21:01:25 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+ num1 = 5
+ num2 = 4
+ addition = 9
 end
 
 def subtraction(num1, num2)
-
+  num1 = 10
+  num2 = 5
+  subtraction = 5
 end
 
 def division(num1, num2)
-
+  num1 = 50
+  num2 = 2
+  division = 25
 end
 
 def multiplication(num1, num2)
-
+  num1 = 4
+  num2 = 30
+  multiplication = 120
 end
 
 def modulo(num1, num2)
-
+  num1 = 34
+  num2 = 5
+  modulo = 4
 end
 
 def square_root(num)
-
+  num = 81
+  square_root = 9
 end
